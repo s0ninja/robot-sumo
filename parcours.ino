dcadb96c952aab5a4eff0dc7c00be890b3624b8f
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-04-29 14:07:15 +0200

    Update and rename parcours.cpp to parcours.ino
    changement est ajout du boutton

--- parcours.ino
+++ parcours.ino
@@ -2,60 +2,74 @@
 #define CAPTEUR_CENTRE 2          // Capteur Centre à la branche 2
 #define CAPTEUR_DROIT 3           // Capteur Droit à la branche 3
 
+#define Roue_DROIT 6
+#define Roue_GAUCHE 5
+
 #define LED_PIN 7                 // Pin de la LED
+
+#define start_button 4
 
 void setup() {
   pinMode(CAPTEUR_GAUCHE, INPUT);
   pinMode(CAPTEUR_CENTRE, INPUT);
   pinMode(CAPTEUR_DROIT, INPUT);
+  pinMode(Roue_GAUCHE, OUTPUT);
+  pinMode(Roue_DROIT, OUTPUT);
+  pinMode(start_button, INPUT);         // Boutton pour start le robot
   pinMode(LED_PIN, OUTPUT);       // Définir le pin de la LED comme une sortie
   Serial.begin(9600);             // Initialisation du moniteur série (vitesse)
 }
 
 void loop() {
-  bool capteurGauche = digitalRead(CAPTEUR_GAUCHE) == HIGH;
-  bool capteurCentre = digitalRead(CAPTEUR_CENTRE) == HIGH;
-  bool capteurDroit = digitalRead(CAPTEUR_DROIT) == HIGH;
+  bool capteurGauche = digitalRead(CAPTEUR_GAUCHE) == true;
+  bool capteurCentre = digitalRead(CAPTEUR_CENTRE) == true;
+  bool capteurDroit = digitalRead(CAPTEUR_DROIT) == true;
+  bool start = digitalRead(start_button) == true;
 
-  if (capteurGauche) {
-    if (capteurCentre) {
-      if (capteurDroit) {
-        digitalWrite(LED_PIN, HIGH);  // Allumer la LED si les capteurs sont dans la bonne configuration
+  if (start_button == true) {
+    digitalWrite(start_button);
+    
+    if (capteurGauche) {
+      if (capteurCentre) {
+        if (capteurDroit) {
+          digitalWrite(LED_PIN, HIGH);  // Allumer la LED si les capteurs sont dans la bonne configuration
+        } else {
+          digitalWrite(Roue_GAUCHE, HIGH);                // Mettre la broche de contrôle du moteur à HIGH pour avancer
+          analogWrite(Roue_GAUCHE, vitesseModérée);       // vitesse du moteur à la valeur |Ligne 10
+        }
       } else {
         digitalWrite(Roue_GAUCHE, HIGH);                // Mettre la broche de contrôle du moteur à HIGH pour avancer
         analogWrite(Roue_GAUCHE, vitesseModérée);       // vitesse du moteur à la valeur |Ligne 10
       }
+    } else if (capteurDroit) {
+      if (capteurCentre) {
+        digitalWrite(Roue_GAUCHE, HIGH);                 // Mettre la broche de contrôle du moteur à HIGH pour avancer
+        analogWrite(Roue_GAUCHE, vitesseMinimal);        // vitesse du moteur à la valeur |Ligne 8
+
+        digitalWrite(Roue_DROIT, HIGH);                  // Mettre la broche de contrôle du moteur à HIGH pour avancer
+        analogWrite(Roue_DROIT, vitesseModérée);         // vitesse du moteur à la valeur |Ligne 10
+      } else {
+        digitalWrite(Roue_GAUCHE, HIGH);                 // Mettre la broche de contrôle du moteur à HIGH pour avancer
+        analogWrite(Roue_GAUCHE, vitesseMinimal);        // vitesse du moteur à la valeur |Ligne 8
+
+        digitalWrite(Roue_DROIT, HIGH);                  // Mettre la broche de contrôle du moteur à HIGH pour avancer
+        analogWrite(Roue_DROIT, vitesseModérée);         // vitesse du moteur à la valeur |Ligne 10
+      }
     } else {
-      digitalWrite(Roue_GAUCHE, HIGH);                // Mettre la broche de contrôle du moteur à HIGH pour avancer
-      analogWrite(Roue_GAUCHE, vitesseModérée);       // vitesse du moteur à la valeur |Ligne 10
-    }
-  } else if (capteurDroit) {
-    if (capteurCentre) {
       digitalWrite(Roue_GAUCHE, HIGH);                 // Mettre la broche de contrôle du moteur à HIGH pour avancer
       analogWrite(Roue_GAUCHE, vitesseMinimal);        // vitesse du moteur à la valeur |Ligne 8
 
       digitalWrite(Roue_DROIT, HIGH);                  // Mettre la broche de contrôle du moteur à HIGH pour avancer
       analogWrite(Roue_DROIT, vitesseModérée);         // vitesse du moteur à la valeur |Ligne 10
-    } else {
-      digitalWrite(Roue_GAUCHE, HIGH);                 // Mettre la broche de contrôle du moteur à HIGH pour avancer
-      analogWrite(Roue_GAUCHE, vitesseMinimal);        // vitesse du moteur à la valeur |Ligne 8
 
-      digitalWrite(Roue_DROIT, HIGH);                  // Mettre la broche de contrôle du moteur à HIGH pour avancer
-      analogWrite(Roue_DROIT, vitesseModérée);         // vitesse du moteur à la valeur |Ligne 10
+      digitalWrite(LED_PIN, HIGH);  // Allumer la LED si les capteurs sont dans la bonne configuration
     }
+
+    digitalWrite(LED_PIN, LOW);  // Eteindre la LED si les capteurs sont dans la bonne configuration
+    delay(1000);  // Ajout d'une pause d'une seconde entre les lectures
   } else {
-    digitalWrite(Roue_GAUCHE, HIGH);                 // Mettre la broche de contrôle du moteur à HIGH pour avancer
-    analogWrite(Roue_GAUCHE, vitesseMinimal);        // vitesse du moteur à la valeur |Ligne 8
-
-    digitalWrite(Roue_DROIT, HIGH);                  // Mettre la broche de contrôle du moteur à HIGH pour avancer
-    analogWrite(Roue_DROIT, vitesseModérée);         // vitesse du moteur à la valeur |Ligne 10
-
-    digitalWrite(LED_PIN, HIGH);  // Allumer la LED si les capteurs sont dans la bonne configuration
+    return;
   }
-
-  digitalWrite(LED_PIN, LOW);  // Eteindre la LED si les capteurs sont dans la bonne configuration
-  delay(1000);  // Ajout d'une pause d'une seconde entre les lectures
-
 }
 
 
