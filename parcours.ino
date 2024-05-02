1f41274a0a6500c5fe9feff50d87c53ed4ff4df5
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-05-02 11:33:40 +0200

    Update parcours.ino
    la lumière marche mais il faut que ça roule

--- parcours.ino
+++ parcours.ino
@@ -1,16 +1,19 @@
-#define CAPTEUR_GAUCHE A0  // Capteur Gauche à la broche A0
-#define CAPTEUR_CENTRE A1  // Capteur Centre à la broche A1
-#define CAPTEUR_DROIT A2   // Capteur Droit à la broche A2
+const int CAPTEUR_GAUCHE = A0; // Capteur Gauche à la broche A0
+const int CAPTEUR_CENTRE = A1;  // Capteur Centre à la broche A1
+const int CAPTEUR_DROIT = A2;   // Capteur Droit à la broche A2
 
-#define Roue_DROITE 12     // Broche du moteur droit
-#define Roue_GAUCHE 13     // Broche du moteur gauche
+const int Roue_DROITE = 12;     // Broche du moteur droit
+const int Roue_GAUCHE = 13;     // Broche du moteur gauche
 
-#define start_button 4     // Bouton pour démarrer le robot
+const int start_button = 4;     // Bouton pour démarrer le robot
+
+const int led = 7;
 
 bool capteurGauche;
 bool capteurCentre;
 bool capteurDroit;
-bool start;
+bool start = false;
+bool etatBouton = false;
 
 void setup() {
   pinMode(CAPTEUR_GAUCHE, INPUT);
@@ -18,24 +21,37 @@ void setup() {
   pinMode(CAPTEUR_DROIT, INPUT);
   pinMode(Roue_GAUCHE, OUTPUT);
   pinMode(Roue_DROITE, OUTPUT);
-  pinMode(start_button, INPUT_PULLUP); // Bouton pour démarrer le robot
+  pinMode(led, OUTPUT);
+  pinMode(start_button, INPUT); // Bouton pour démarrer le robot
 }
 
 void loop() {
   capteurGauche = digitalRead(CAPTEUR_GAUCHE);
   capteurCentre = digitalRead(CAPTEUR_CENTRE);
   capteurDroit = digitalRead(CAPTEUR_DROIT);
-  start = digitalRead(start_button);
+  etatBouton = digitalRead(start_button);
 
-  if (start) {
-    if (capteurGauche && capteurCentre && capteurDroit) {
+  if (etatBouton == true) {
+    if (start == true) {
+      start = false;
+      delay(500);
+    }
+    else {
+      start = true;
+      delay(500);
+    }
+  }
+
+  if (start == true) {
+    digitalWrite(led, HIGH);
+    if (capteurGauche, HIGH & capteurCentre, HIGH & capteurDroit, HIGH) {
       digitalWrite(Roue_GAUCHE, HIGH);
       digitalWrite(Roue_DROITE, HIGH);
     } else {
-      if (capteurGauche) {
+      if (capteurGauche, HIGH) {
         digitalWrite(Roue_GAUCHE, LOW);
         digitalWrite(Roue_DROITE, HIGH);
-      } else if (capteurDroit) {
+      } else if (capteurDroit, HIGH) {
         digitalWrite(Roue_GAUCHE, HIGH);
         digitalWrite(Roue_DROITE, LOW);
       } else {
@@ -44,6 +60,7 @@ void loop() {
       }
     }
   } else {
+    digitalWrite(led, LOW);
     digitalWrite(Roue_GAUCHE, LOW);
     digitalWrite(Roue_DROITE, LOW);
   }
