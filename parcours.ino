5ce55a5ae1f1f760c643ca95a290a1a99a84365b
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-05-13 14:24:14 +0200

    Update parcours.ino

--- parcours.ino
+++ parcours.ino
@@ -1,119 +1,111 @@
-const int CAPTEUR_GAUCHE = 5; // Capteur Gauche à la broche A0
-const int CAPTEUR_CENTRE = 6; // Capteur Centre à la broche A1
-const int CAPTEUR_DROIT = 2; // Capteur Droit à la broche A2
+const int linefinderGauche = 2;
+const int linefinderDroite = 4;
+const int linefinderCentre = A3;
+const int boutonMarche = A2;
 
-const int vitesseDroite = 11;
-const int vitesseGauche = 3;
-const int sensDroite = 13;
-const int sensGauche = 12;
-const int freinDroite = 8;
-const int freinGauche = 9;
+const int sensMGauche = 12;
+const int marcheGauche = 9;
+const int puissanceGauche = 3;
 
-const int start_button = 4; // Bouton pour démarrer le robot
+const int sensMDroit = 13;
+const int marcheDroit = 8;
+const int puissanceDroit = 11;
 
-const int led = 7;
+const int AFOND = 130;
+const int PRUDENT = 60;
+const int NBMESURE = 4;
 
-bool capteurGauche;
-bool capteurCentre;
-bool capteurDroit;
-bool start = false;
-bool etatBouton = false;
+bool LFG = false, LFD = false, LFC = false;
+
+void arret() {
+  // S'arrête
+  digitalWrite(marcheGauche, LOW);
+  digitalWrite(marcheDroit, LOW);
+}
+
+void moteurDroite(int vitesse) {
+  //Tourne à droite
+  digitalWrite(sensMDroit, HIGH);
+  analogWrite(puissanceDroit, vitesse);
+}
+
+void moteurGauche(int vitesse) {
+  //Tourne a gauche
+  digitalWrite(sensMGauche, HIGH);
+  analogWrite(puissanceGauche, vitesse);
+}
+
+void moteurDroiteR(int vitesse) {
+  //Tourne à droite en reculant
+  digitalWrite(sensMDroit, LOW);
+  analogWrite(puissanceDroit, vitesse);
+}
+
+void moteurGaucheR(int vitesse) {
+  //Tourne à gauche en reculant
+  digitalWrite(sensMGauche, LOW);
+  analogWrite(puissanceGauche, vitesse);
+}
+
+byte acquisition() {
+  LFG = digitalRead(linefinderGauche);
+  LFD = digitalRead(linefinderDroite);
+  LFC = digitalRead(linefinderCentre);
+if 
+  if (LFD == LOW && LFG == LOW && LFC == LOW) {
+    return 0;
+  } else if (LFD == LOW && LFC == LOW) {
+    return 1;
+  } else if (LFG == LOW && LFC == LOW) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
 
 void setup() {
-  pinMode(CAPTEUR_GAUCHE, INPUT);
-  pinMode(CAPTEUR_CENTRE, INPUT);
-  pinMode(CAPTEUR_DROIT, INPUT);
+  pinMode(linefinderGauche, INPUT);
+  pinMode(linefinderDroite, INPUT);
+  pinMode(linefinderCentre, INPUT);
+  pinMode(boutonMarche, INPUT);
+  
+  pinMode(sensMGauche, OUTPUT);
+  pinMode(marcheGauche, OUTPUT);
+  pinMode(puissanceGauche, OUTPUT);
 
-  pinMode(vitesseDroite, OUTPUT);
-  pinMode(vitesseGauche, OUTPUT);
-  pinMode(sensDroite, OUTPUT);
-  pinMode(sensGauche, OUTPUT);
-  pinMode(freinDroite, OUTPUT);
-  pinMode(freinGauche, OUTPUT);
-
-  pinMode(led, OUTPUT);
-  pinMode(start_button, INPUT); // Bouton pour démarrer le robot
+  pinMode(sensMDroit, OUTPUT);
+  pinMode(marcheDroit, OUTPUT);
+  pinMode(puissanceDroit, OUTPUT);
 }
 
 void loop() {
-  capteurGauche = digitalRead(CAPTEUR_GAUCHE);
-  capteurCentre = digitalRead(CAPTEUR_CENTRE);
-  capteurDroit = digitalRead(CAPTEUR_DROIT);
-  etatBouton = digitalRead(start_button);
-  
-ss
-  if (etatBouton == true) {
-    if (start == false) {
-      start = true;
-      delay(500);
-    }
-    else {
-      start = false;
-      delay(500);
-    }
+  switch (acquisition()) {
+    case 0:
+      arret();
+      break;
+    case 1:
+      moteurDroite(AFOND);
+      moteurGaucheR(AFOND);
+      break;
+    case 2:
+      moteurDroiteR(AFOND);
+      moteurGauche(AFOND);
+      break;
+
+    case 3 :
+      moteurDroite(AFOND);
+      moteurGaucheR(AFOND);
+
+      moteurDroiteR(AFOND);
+      moteurGauche(AFOND);
+      break;
+
+    default:
+      arret();
+      break;
   }
-
-  if (start = true) {
-    digitalWrite(7, HIGH);
-    digitalWrite(4, HIGH);
-  } else {
-    digitalWrite(4, LOW);
-  }
-
-  if (capteurGauche && capteurCentre && capteurDroit) {
-    digitalWrite(7, HIGH);
-    digitalWrite(4, HIGH);
-
-    digitalWrite(13, HIGH); // Sens Droit Avancer
-    digitalWrite(12, HIGH); //Sens Gauche Avancer
-
-    analogWrite(11, 255); //Vitesse 255 Droit
-    analogWrite(3, 255); //Vitesse 255 Gauche
-
-    digitalWrite(8, LOW); //Frein non engagé Droit
-    digitalWrite(9, LOW); //Frein non engagé Gauche
-  } else {
-    if (capteurGauche) {
-      digitalWrite(13, HIGH); // Sens Droit Avancer
-      digitalWrite(12, HIGH); //Sens Gauche Avancer
-
-      analogWrite(11, 255); //Vitesse 255 Droit
-      analogWrite(3, 0); //Vitesse 0 Gauche
-
-      digitalWrite(8, HIGH); //Frein engagé Droit
-      digitalWrite(9, LOW); //Frein non engagé Gauche
-    } else if (capteurDroit) {
-      digitalWrite(13, HIGH); // Sens Droit Avancer
-      digitalWrite(12, HIGH); //Sens Gauche Avancer
-
-      analogWrite(11, 0); //Vitesse 0 Droit
-      analogWrite(3, 255); //Vitesse 255 Gauche
-
-      digitalWrite(8, LOW); //Frein non engagé Droit
-      digitalWrite(9, HIGH); //Frein engagé Gauche
-    } else {
-      digitalWrite(13, LOW); // Sens Droit Avancer
-      digitalWrite(12, LOW); //Sens Gauche Avancer
-
-      analogWrite(11, 0); //Vitesse 0 Droit
-      analogWrite(3, 0); //Vitesse 0 Gauche
-
-      digitalWrite(8, HIGH); //Frein engagé Droit
-      digitalWrite(9, HIGH); //Frein engagé Gauche
-    }
-  }
-  digitalWrite(4, LOW);
-  digitalWrite(7, LOW);
-
-  digitalWrite(13, LOW); // Sens Droit Avancer
-  digitalWrite(12, LOW); //Sens Gauche Avancer
-
-  analogWrite(11, 0); //Vitesse 0 Droit
-  analogWrite(3, 0); //Vitesse 0 Gauche
-
-  digitalWrite(8, HIGH); //Frein engagé Droit
-  digitalWrite(9, HIGH); //Frein engagé Gauche
 }
+
 
 /*
  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____
