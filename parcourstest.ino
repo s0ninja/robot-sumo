f95c094d90a858cb92e0aa1ff520b8d15df8c200
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-06-03 15:53:46 +0200

    Update parcourstest.ino

--- parcourstest.ino
+++ parcourstest.ino
@@ -2,7 +2,7 @@
 #define capteurDeLigneDroite 4
 #define capteurDeLigneCentre 7
 
-#define boutonMarche 6
+#define bouton A2
 #define led 5
 
 #define sensMoteurGauche 12
@@ -13,56 +13,60 @@
 #define marcheMoteurDroite 8
 #define puissanceMoteurDroite 11
 
-#define AFOND 130
-#define PRUDENT 60
+byte SPEED = 255, AFOND = 175, nul = 0;
 
-bool LFD = 0, LFC = 0, LFG = 0;
+bool sensMoteurGauche = LOW;
+bool sensMoteurDroit = LOW;
+
+bool sensMoteurGenerale = LOW;
+
+bool CapG = 0, CapD = 0, CapC = 0;
+bool etatBouton = 0, marche = 0;
 int X = 0;
 
 void arret(int v) {
   digitalWrite(sensMoteurGauche, HIGH); 
-  digitalWrite(marcheMoteurGauche, LOW);   
+  digitalWrite(marcheMoteurGauche, HIGH);   
   analogWrite(puissanceMoteurGauche, v);
-
-  digitalWrite(sensMoteurDroite, HIGH);
-  digitalWrite(marcheMoteurDroite, LOW);   
+  digitalWrite(sensMoteurDroite, LOW);
+  digitalWrite(marcheMoteurDroite, HIGH);   
   analogWrite(puissanceMoteurDroite, v);
 }
 
 void allergauche(int v) {
-  digitalWrite(sensMoteurGauche, HIGH);
+  digitalWrite(sensMoteurGauche, sensMoteurGenerale);
   analogWrite(puissanceMoteurGauche, v);
+  digitalWrite(marcheMoteurGauche, LOW);
+
+  digitalWrite(sensMoteurDroite, sensMoteurGenerale);
+  analogWrite(puissanceMoteurDroite, nul);
+  digitalWrite(marcheMoteurDroite, HIGH);
+}
+
+void allerdroite(int v) {
+  digitalWrite(sensMoteurGauche, sensMoteurGenerale);
+  analogWrite(puissanceMoteurGauche, nul);
   digitalWrite(marcheMoteurGauche, HIGH);
 
-  digitalWrite(sensMoteurDroite, LOW);
+  digitalWrite(sensMoteurDroite, sensMoteurGenerale);
   analogWrite(puissanceMoteurDroite, v);
   digitalWrite(marcheMoteurDroite, LOW);
 }
 
-void allerdroite(int v) {
-  digitalWrite(sensMoteurGauche, LOW);
+void avancer(int v) {
+  digitalWrite(sensMoteurGauche, sensMoteurGenerale);
   analogWrite(puissanceMoteurGauche, v);
   digitalWrite(marcheMoteurGauche, LOW);
-
-  digitalWrite(sensMoteurDroite, HIGH);
+  digitalWrite(sensMoteurDroite, sensMoteurGenerale);
   analogWrite(puissanceMoteurDroite, v);
-  digitalWrite(marcheMoteurDroite, HIGH);
-}
-
-void avancer(int v) {
-  digitalWrite(sensMoteurGauche, HIGH);
-  analogWrite(puissanceMoteurGauche, v);
-  digitalWrite(marcheMoteurGauche, HIGH);
-
-  digitalWrite(sensMoteurDroite, HIGH);
-  analogWrite(puissanceMoteurDroite, v);
-  digitalWrite(marcheMoteurDroite, HIGH);
+  digitalWrite(marcheMoteurDroite, LOW);
 }
 
 byte acquisition() {
   LFD = digitalRead(capteurDeLigneDroite);
   LFG = digitalRead(capteurDeLigneGauche);
   LFC = digitalRead(capteurDeLigneCentre);
+
 
   if ((LFG == 0) && (LFC == 0) && (LFD == 0)) {
     return 0; // Avancer
@@ -88,40 +92,55 @@ void setup() {
 
   pinMode(capteurDeLigneGauche, INPUT);
   pinMode(capteurDeLigneDroite, INPUT);
-  pinMode(capteurDeLigneCentre, INPUT);
 
   pinMode(led, OUTPUT);
+  pinMode(bouton, INPUT);
 
+  //pinMode(capteurDeLigneCentre, INPUT);
   Serial.begin(9600);
 
-  do {
-    X = analogRead(boutonMarche);
-    delay(20);
-  } while (X < 1000);
 }
 
-
 void loop() {
+  etatBouton = analogRead(bouton)
+
+  if (etatBouton == 1) {
+    if (marche == 0) {
+      marche = 1;
+    } else {
+      marche = 0;
+    }
+    delay(500);
+  }
+
+  Serial.println(marche);
+
+  if (marche == 1) {
+    analogWrite(led, HIGH);
   switch (acquisition()) {
-    case 0:
+    case 0:                         // Case Avancer
       avancer(AFOND);
       Serial.println("case 0, avancer");
       break;
-    case 1:
-      arret(AFOND);
+    case 1:      // Case Stopper
+      arret(nul);                   
       Serial.println("case 1, Arret");
       break;
-    case 2:
+    case 2:                         // Case Aller à droite
       allerdroite(AFOND);
       Serial.println("case 2, tourner à droite");
       break;
-    case 3:
+    case 3:                         // Case Aller à gauche
       allergauche(AFOND);
       Serial.println("case 3, tourner à gauche");
       break;
-    default:
-      arret(AFOND);
+    default:                        // Case par Défaut
+      arret(nul);
       Serial.println("défault");
       break;
   }
+  } else {
+    analogWrite(led, LOW);
+  }
+  delay(40);  
 }
