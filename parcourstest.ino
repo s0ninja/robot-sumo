145c8fd86b990e442dee10985c191e5ad4b1f8ef
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-06-03 14:45:29 +0200

    Update parcourstest.ino

--- parcourstest.ino
+++ parcourstest.ino
@@ -1,115 +1,164 @@
-#define linefinderGauche 2
-#define linefinderDroite 4
-#define linefinderCentre 7
-#define boutonMarche A2
-#define sensMGauche 12
-#define marcheGauche 9
-#define puissanceGauche 3
-#define sensMDroit 13
-#define marcheDroit 8
-#define puissanceDroit 11
+#define capteurDeLigneGauche 2
+#define capteurDeLigneDroite 4
+#define capteurDeLigneCentre 7
 
- byte SPEED = 255, AFOND = 175, nul = 0;
+#define boutonMarche 6
+#define led 5
 
-bool LFG = 0, LFD = 0, LFC = 0;
+#define sensMoteurGauche 12
+#define marcheMoteurGauche 9
+#define puissanceMoteurGauche 3
+
+#define sensMoteurDroite 13
+#define marcheMoteurDroite 8
+#define puissanceMoteurDroite 11
+
+#define AFOND 130
+#define PRUDENT 60
+
+bool LFD = 0, LFC = 0, LFG = 0;
 int X = 0;
 
 void arret(int v) {
-  digitalWrite(sensMGauche, HIGH); 
-  digitalWrite(marcheGauche, HIGH);   
-  analogWrite(puissanceGauche, v);
-  digitalWrite(sensMDroit, LOW);
-  digitalWrite(marcheDroit, HIGH);   
-  analogWrite(puissanceDroit, v);
+  digitalWrite(sensMoteurGauche, HIGH); 
+  digitalWrite(marcheMoteurGauche, LOW);   
+  analogWrite(puissanceMoteurGauche, v);
 
-  delay(200);
+  digitalWrite(sensMoteurDroite, HIGH);
+  digitalWrite(marcheMoteurDroite, LOW);   
+  analogWrite(puissanceMoteurDroite, v);
 }
 
 void allergauche(int v) {
-  digitalWrite(sensMGauche, HIGH);
-  analogWrite(puissanceGauche, v);
-  digitalWrite(marcheGauche, LOW);
+  digitalWrite(sensMoteurGauche, HIGH);
+  analogWrite(puissanceMoteurGauche, v);
+  digitalWrite(marcheMoteurGauche, HIGH);
 
-  delay(200);
+  digitalWrite(sensMoteurDroite, LOW);
+  analogWrite(puissanceMoteurDroite, v);
+  digitalWrite(marcheMoteurDroite, LOW);
 }
 
 void allerdroite(int v) {
-  digitalWrite(sensMDroit, LOW);
-  analogWrite(puissanceDroit, v);
-  digitalWrite(marcheDroit, LOW);
+  digitalWrite(sensMoteurGauche, LOW);
+  analogWrite(puissanceMoteurGauche, v);
+  digitalWrite(marcheMoteurGauche, LOW);
 
-  delay(200);
+  digitalWrite(sensMoteurDroite, HIGH);
+  analogWrite(puissanceMoteurDroite, v);
+  digitalWrite(marcheMoteurDroite, HIGH);
 }
 
 void avancer(int v) {
-  digitalWrite(sensMGauche, HIGH);
-  analogWrite(puissanceGauche, v);
-  digitalWrite(marcheGauche, LOW);
-  digitalWrite(sensMDroit, LOW);
-  analogWrite(puissanceDroit, v);
-  digitalWrite(marcheDroit, LOW);
+  digitalWrite(sensMoteurGauche, HIGH);
+  analogWrite(puissanceMoteurGauche, v);
+  digitalWrite(marcheMoteurGauche, HIGH);
 
-  delay(200);
+  digitalWrite(sensMoteurDroite, HIGH);
+  analogWrite(puissanceMoteurDroite, v);
+  digitalWrite(marcheMoteurDroite, HIGH);
 }
 
 byte acquisition() {
-  LFD = digitalRead(linefinderDroite);
-  LFG = digitalRead(linefinderGauche);
-  LFC = digitalRead(linefinderCentre);
+  LFD = digitalRead(capteurDeLigneDroite);
+  LFG = digitalRead(capteurDeLigneGauche);
+  LFC = digitalRead(capteurDeLigneCentre);
 
-  if ((LFG == 0) && (LFC == 0) && (LFD == 0)) {   // Avancer
+  if ((LFG == 0) && (LFC == 0) && (LFD == 0)) {
     return 0; // Avancer
-  } else if (((LFG == 1) && (LFC == 1) && (LFD == 0))||((LFG == 1) && (LFC == 0) && (LFD == 0))) { // Allez à Gauche
-    return 2; // Allez à Gauche
-  } else if (((LFG == 0) && (LFC == 1) && (LFD == 1))||((LFG == 0) && (LFC == 0) && (LFD == 1))) { // Allez à Droite
-    return 1; // Allez à Droite
-  } else if ((LFG == 1) && (LFC == 0) && (LFD == 1)) { // Avancer
-    return 3; // STOP
-  } else if (((LFG == 1) && (LFC == 0) && (LFD == 1))||((LFG == 1) && (LFC == 1) && (LFD == 1))) { // Cas Impossible
-    return 3; // STOP
+  } else if (((LFG == 1) && (LFC == 1) && (LFD == 0)) || ((LFG == 1) && (LFC == 0) && (LFD == 0))) {
+    return 3; // Allez à Gauche
+  } else if (((LFG == 0) && (LFC == 1) && (LFD == 1)) || ((LFG == 0) && (LFC == 0) && (LFD == 1))) {
+    return 2; // Allez à Droite
+  } else if ((LFG == 1) && (LFC == 0) && (LFD == 1)) {
+    return 1; // STOP
+  } else {
+    return 1; // STOP
   }
-  return 100;
 }
 
 void setup() {
-  pinMode(sensMGauche, OUTPUT);
-  pinMode(marcheGauche, OUTPUT);
-  pinMode(puissanceGauche, OUTPUT);
-  pinMode(sensMDroit, OUTPUT);
-  pinMode(marcheDroit, OUTPUT);
-  pinMode(puissanceDroit, OUTPUT);
-  pinMode(linefinderGauche, INPUT);
-  pinMode(linefinderDroite, INPUT);
-  pinMode(linefinderCentre, INPUT);
+  pinMode(sensMoteurGauche, OUTPUT);
+  pinMode(marcheMoteurGauche, OUTPUT);
+  pinMode(puissanceMoteurGauche, OUTPUT);
+
+  pinMode(sensMoteurDroite, OUTPUT);
+  pinMode(marcheMoteurDroite, OUTPUT);
+  pinMode(puissanceMoteurDroite, OUTPUT);
+
+  pinMode(capteurDeLigneGauche, INPUT);
+  pinMode(capteurDeLigneDroite, INPUT);
+  pinMode(capteurDeLigneCentre, INPUT);
+
+  pinMode(led, OUTPUT);
+
   Serial.begin(9600);
 
-  do {
-    X = analogRead(boutonMarche);
-    delay(20);
-  } while (X < 1000);
+  do {X  = analogRead(boutonMarche);
+    delay(20);}
+  while (X<1000);                       
 }
+
 
 void loop() {
   switch (acquisition()) {
-    case 0:                         // Case Avancer
+    case 0:
       avancer(AFOND);
-      Serial.println("case 0");
+      Serial.println("case 0, avancer");
       break;
-    case 1:                         // Case Allez à droite
+    case 1:
+      arret(AFOND);
+      Serial.println("case 1, Arret");
+      break;
+    case 2:
       allerdroite(AFOND);
-      Serial.println("case 1");
+      Serial.println("case 2, tourner à droite");
       break;
-    case 2:                         // Case Allez à Gauche
+    case 3:
       allergauche(AFOND);
-      Serial.println("case 2");
+      Serial.println("case 3, tourner à gauche");
       break;
-    case 3:                         // Case Stop
-      arret(nul);
-      Serial.println("case 3");
-      break;
-    default:                        // Case par Défault
-      arret(nul);
+    default:
+      arret(AFOND);
       Serial.println("défault");
       break;
   }
 }
+
+/*
+ _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____
+|_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____|
+  ____  ____    ___   _   _  ____   _____   _____
+ / ___||  _ \  / _ \ | | | ||  _ \ | ____| |___  |
+| |  _ | |_) || | | || | | || |_) ||  _|      / /
+| |_| ||  _ < | |_| || |_| ||  __/ | |___    / /
+ \____||_| \_\ \___/  \___/ |_|    |_____|  /_/
+ _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____
+|_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____|
+ __  __               _
+|  \/  |  __ _  _ __ (_) _ __    ___
+| |\/| | / _` || '__|| || '_ \  / _ \
+| |  | || (_| || |   | || | | ||  __/
+|_|  |_| \__,_||_|   |_||_| |_| \___|
+ ____
+|  _ \   ___   _ __ ___   _   _
+| |_) | / _ \ | '_ ` _ \ | | | |
+|  _ < | (_) || | | | | || |_| |
+|_| \_\ \___/ |_| |_| |_| \__, |
+                          |___/
+ ____   _
+/ ___| | |_   ___ __   __  ___  _ __ 
+\___ \ | __| / _ \\ \ / / / _ \| '_ \
+ ___) || |_ |  __/ \ V / |  __/| | | |
+|____/  \__| \___|  \_/   \___||_| |_|
+ ____
+/ ___|   ___   _ __   _   _
+\___ \  / _ \ | '_ \ | | | |
+ ___) || (_) || | | || |_| |
+|____/  \___/ |_| |_| \__, |
+                      |___/
+ _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____  _____
+|_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____||_____|
+
+*/
+
