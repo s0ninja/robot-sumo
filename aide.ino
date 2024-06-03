3e57b2a47a1af1c4165bea84fcc27c74d80cce58
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-06-03 15:00:00 +0200

    Update and rename README.ino to aide.ino

--- aide.ino
+++ aide.ino
@@ -1,4 +1,9 @@
+#define capteurGauche 5 //ça sert a définir mon capteur gauche sur la broche 5
+
 digitalWrite( , ); //Faire l'information avec une information 0 ou  1
 analogWrite( , ); //Faire l'information entre une information 0 à 255
 
 digitalRead( , ); //Lire une information 
+
+if(){} //Si
+else(){} //Sinon
