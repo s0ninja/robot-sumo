e06a8f069e5160adf929d978933980863ec42517
s0ninja <145279686+s0ninja@users.noreply.github.com>
2024-06-03 15:07:46 +0200

    Update aide.ino

--- aide.ino
+++ aide.ino
@@ -1,4 +1,4 @@
-#define capteurGauche 5 //ça sert a définir mon capteur gauche sur la broche 5
+#define mon_nom 5 //ça sert a définir mon capteur gauche sur la broche 5
 
 digitalWrite( , ); //Faire l'information avec une information 0 ou  1
 analogWrite( , ); //Faire l'information entre une information 0 à 255
